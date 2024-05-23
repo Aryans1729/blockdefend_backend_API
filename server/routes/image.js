b73d094dcafa4c9463f417b64a3672cc2dfdb2d4
Aryan Sharma <aryansharma@Aryans-MacBook-Air.local>
2024-05-24 01:06:59 +0530

    fix(image-API): fix image APi for placeAd field

--- server/routes/image.js
+++ server/routes/image.js
@@ -6,10 +6,25 @@ const router = express.Router();
 router.use(express.json());
 
 
-/// Route to serve the image
-router.get('/image', authenticateApiKey,(req, res) => {
-    const imagePath = path.join(__dirname, '../../images', 'chrome-extension-image.jpg');
-    res.sendFile(imagePath);
+// /// Route to serve the image
+// router.get('/image', authenticateApiKey,(req, res) => {
+//     const imagePath = path.join(__dirname, '../../images', 'chrome-extension-image.jpg');
+//     res.sendFile(imagePath);
+// });
+
+const imageUrl = "https://techpp.com/wp-content/uploads/2023/02/Best-image-downloader-chrome-extensions.jpg";  // Replace this with the actual URL of your image
+
+router.get('/get_data', (req, res) => {
+    const placeAd = req.query.placeAd === 'true';
+    const image = placeAd ? imageUrl : '';
+
+    const response = {
+        image: image,
+        placeAd: placeAd
+    };
+
+    res.json(response);
 });
+
 
 module.exports = router;
